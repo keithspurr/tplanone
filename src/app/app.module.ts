bd77333ea427a4f45f17b813a0c2f32809dc8f31
keithspurr <keithspurr@ksqdesign.com>
2021-06-20 14:40:21 +0100

    flex layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MaterialModule } from './material.module';
 import { TplantreeoneComponent } from './components/tplantreeone/tplantreeone.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   entryComponents: [],
